ed0e4e9b176a7645dc0559216404b3d2f335f931
Zack Mayeda <zack.mayeda@gmail.com>
2013-02-21 11:27:02 -0800

    added intro comments

--- main.cpp
+++ main.cpp
@@ -1,4 +1,27 @@
+/*
+	CS184 Assignment 2 - Ray Tracing
+	Tyler Brabham cs184-ej
+	Zack Mayeda cs184-bg
+*/
 
+/*
+	Required Features
+	-----------------
+	- render arbitrarily oriented ellipsoids (spheres are sufficient
+	primitives as long as you properly apply scales and rotations)
+	- render polygons (read .obj file)
+	- simple Phong Shading (in color)
+	- compute shadows
+	- compute reflections
+	- apply linear transformations to objects
+	- use point and directional lights
+	- write output to standard image format
+
+	Input Format
+	_________________
+	- follows last semester example
+	
+*/
 
 int main(int argc, char *argv[]) {
 	
