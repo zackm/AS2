39c235e5ed60b3234eed8c1461b29c6bd0c62f13
TylerBrabham <tylerbrabham@hotmail.com>
2013-02-24 02:01:43 -0800

    tested and changed triangles

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,7 +1,7 @@
 #include "Triangle.h"
 #include "glm/glm.hpp"
-#include <iostream>
 #include "glm/gtc/matrix_inverse.hpp"
+#include <iostream>
 
 using namespace std;
 
@@ -12,7 +12,51 @@ Triangle::Triangle(glm::vec3 arg_a,glm::vec3 arg_b,glm::vec3 arg_c){
 }
 
 bool Triangle::intersect(Ray& ray, float* thit, LocalGeo* local){
-	return false;
+	if (!intersect(ray)){
+		return false;
+	}else{
+
+		glm::vec3 plane_vec1 = b-a;
+		glm::vec3 plane_vec2 = c-a;
+
+		glm::vec3 normal = glm::cross(plane_vec1,plane_vec2);
+		normal /= glm::sqrt(glm::dot(normal,normal));
+		float denom = glm::dot(ray.direction,normal);
+
+		float t = -(glm::dot(ray.position-a,normal))/(denom);
+
+		float beta, gamma;
+
+		glm::vec3 point = ray.position+t*ray.direction;
+
+		//beta,gamma, t.
+		glm::vec3 matrix_point = ray.position-a; //these formulae from slides
+
+		//glm::mat3 bary_matrix(plane_vec1[0],plane_vec1[1],plane_vec1[2],plane_vec2[0],
+		//					  plane_vec2[1],plane_vec2[2],-ray.direction[0],-ray.direction[1],
+		//					  -ray.direction[2]); //test matrix for row or column first constructor
+
+
+		//I believe this to be correct for mat3 constructor
+		glm::mat3 bary_matrix(plane_vec1[0],plane_vec2[0],-ray.direction[0],plane_vec1[1],
+			plane_vec2[1],-ray.direction[1],plane_vec1[2],plane_vec2[2],
+			-ray.direction[2]); //test matrix for row or column first constructor
+
+		glm::mat3 matrix_inv = bary_matrix._inverse();
+
+		glm::vec3 sol = matrix_inv*matrix_point;
+		beta = sol[0];
+		gamma = sol[1];
+
+		if (beta>=0 && gamma>=0 && (beta+gamma<=1)){ //<= is to ensure alpha is also >=0.
+			*thit = sol[2];
+			local->normal = normal;
+			local->point = ray.position+*thit*ray.direction;
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
 
 bool Triangle::intersect(Ray& ray){
@@ -22,9 +66,9 @@ bool Triangle::intersect(Ray& ray){
 	glm::vec3 plane_vec2 = c-a;
 
 	glm::vec3 normal = glm::cross(plane_vec1,plane_vec2);
-	normal /= glm::dot(normal,normal);
+	normal /= glm::sqrt(glm::dot(normal,normal));
 
-	float denom = glm::dot(ray.direction,normal);
+	float denom = glm::sqrt(glm::dot(ray.direction,normal));
 	if (denom==0){
 		return false;
 	}else{
@@ -38,20 +82,24 @@ bool Triangle::intersect(Ray& ray){
 
 			//beta,gamma, t.
 			glm::vec3 matrix_point = ray.position-a; //these formulae from slides
-			glm::mat3 bary_matrix(plane_vec1[0],plane_vec1[1],plane_vec1[2],plane_vec2[0],
-								  plane_vec2[1],plane_vec2[2],-ray.direction[0],-ray.direction[1],
-								  -ray.direction[2]); //test matrix for row or column first constructor
-			glm::mat3 matrix_inv = bary_matrix._inverse();
 
-			//cout<<matrix_inv[1][2]<<'\n';
-			//cout<<bary_matrix[0][0];
+			//glm::mat3 bary_matrix(plane_vec1[0],plane_vec1[1],plane_vec1[2],plane_vec2[0],
+			//					  plane_vec2[1],plane_vec2[2],-ray.direction[0],-ray.direction[1],
+			//					  -ray.direction[2]); //test matrix for row or column first constructor
+
+
+			//I believe this to be correct for mat3 constructor
+			glm::mat3 bary_matrix(plane_vec1[0],plane_vec2[0],-ray.direction[0],plane_vec1[1],
+				plane_vec2[1],-ray.direction[1],plane_vec1[2],plane_vec2[2],
+				-ray.direction[2]); //test matrix for row or column first constructor
+			glm::mat3 matrix_inv = bary_matrix._inverse();
 
 			glm::vec3 sol = matrix_inv*matrix_point;
 			beta = sol[0];
 			gamma = sol[1];
 			thit = sol[2];
 
-			if (beta>=0 && gamma>=0 && (beta+gamma<=1)){
+			if (beta>=0 && gamma>=0 && (beta+gamma<=1)){ //<= is to ensure alpha is also >=0.
 				return true;
 			}else{
 				return false;
@@ -61,17 +109,20 @@ bool Triangle::intersect(Ray& ray){
 }
 
 //int main (int argc, char* argv[]){
-//	glm::vec3 v1(0,0,0);
-//	glm::vec3 v2(1,0,0);
-//	glm::vec3 v3(0,1,0);
+//	glm::vec3 v1(0.0f,0.0f,1.0f);
+//	glm::vec3 v2(1.0f,1.0f,.5f);
+//	glm::vec3 v3(0.0f,1.0f,0.0f);
 //
-//	glm::vec3 pos(.5,0,5);
-//	glm::vec3 dir(0,0,-1);
+//	glm::vec3 pos(0.0f,0.0f,5.0f);
+//	glm::vec3 dir(0.0f,0.0f,-1);
 //
 //	Ray testRay(pos,dir,0,10);
 //
 //	Triangle testTri(v1,v2,v3);
-//	cout<<testTri.intersect(testRay);
+//	float thit = 0;
+//	LocalGeo localTest;
+//	testTri.intersect(testRay,&thit,&localTest);
+//	cout<<localTest.normal[2];
 //
 //	cin.get();
 //	return 0;
