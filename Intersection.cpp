6edea3bb0bbd3ee7f01f00bf6a2ab9459f210cf7
Zack Mayeda <zack.mayeda@gmail.com>
2013-02-27 18:22:03 -0800

    removing unused files

--- Intersection.cpp
+++
@@ -1,6 +0,0 @@
-#include "Intersection.h"
-
-Intersection::Intersection(LocalGeo local,Primitive* prime){
-	localGeo = local;
-	primitive = prime;
-}
--- Sample.h
+++
@@ -1,8 +0,0 @@
-
-class Sample {
-public:
-	float x,y;
-
-	Sample(){};
-	Sample(float,float);
-};
